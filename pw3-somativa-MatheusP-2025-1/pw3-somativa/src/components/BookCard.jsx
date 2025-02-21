b7bcc8ced8773813e36f760e716789be4cb4ecf9
Matheusmp3 <167825465+Matheusmp3@users.noreply.github.com>
2025-02-20 21:58:56 -0300

    Add files via upload

--- pw3-somativa-MatheusP-2025-1/pw3-somativa/src/components/BookCard.jsx
+++ pw3-somativa-MatheusP-2025-1/pw3-somativa/src/components/BookCard.jsx
@@ -1,9 +1,13 @@
+import Button from './Button.jsx'
+import style from "./BookCard.module.css"
+
 const BookCard = ({titulo, autor, imagem})=>{
     return(
-        <div>
-            <h3>{titulo}</h3>
-            <p>{autor}</p>
+        <div className={style.bookCard}>
+            <h3  className={style.titulo}>{titulo}</h3>
+            <p  className={style.author}>{autor}</p>
            <img src={imagem} alt="Capa: As Cavernas de Aço"/>
+           <Button label='Detalhe'/>
         </div>
     )
 }
